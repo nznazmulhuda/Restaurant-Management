c7a6dd745b3fc8bc8786545db91f51b7137be985
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-05-11 15:44:51 +0600

    404 page added

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,29 @@
+import Lottie from "react-lottie";
+import Error from "../../assets/404.json";
+import { Link } from "react-router-dom";
+
 function ErrorPage() {
-  return (
-    <div>
-      <h1>Error Page</h1>
-    </div>
-  );
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: Error,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    };
+    return (
+        <div className="relative">
+            <div className="absolute top-0 left-0">
+                <Lottie options={defaultOptions} />
+            </div>
+            <Link
+                to={"/"}
+                className="absolute top-10 left-10 text-xl md:text-2xl lg:text-3xl font-bold underline"
+            >
+                Back to home
+            </Link>
+        </div>
+    );
 }
 
 export default ErrorPage;
