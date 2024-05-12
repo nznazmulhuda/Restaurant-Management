d1c3f3e564cf8a7d31ea9fc30f2d61aae4adcdd2
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-05-12 13:20:06 +0600

    pagination added and data loaded when page change

---
+++ src/Components/Paginations.jsx
@@ -0,0 +1,28 @@
+import { Pagination } from "rsuite";
+import PropTypes from "prop-types";
+
+function Paginations({ total, setActivePage, activePage }) {
+    return (
+        <>
+            <Pagination
+                prev
+                last
+                next
+                first
+                size="lg"
+                total={total}
+                limit={10}
+                activePage={activePage}
+                onChangePage={setActivePage}
+            />
+        </>
+    );
+}
+
+Paginations.propTypes = {
+    total: PropTypes.number,
+    setActivePage: PropTypes.func,
+    activePage: PropTypes.number,
+};
+
+export default Paginations;
--- src/Pages/AllFood/AllFood.jsx
+++ src/Pages/AllFood/AllFood.jsx
@@ -1,11 +1,15 @@
-import { useEffect, useState } from "react";
-import Title from "../../Components/Title";
-import { FaSearch } from "react-icons/fa";
-import FoodCard from "../../Components/FoodCard";
 import axios from "axios";
+import { FaSearch } from "react-icons/fa";
+import Title from "../../Components/Title";
+import { useEffect, useState } from "react";
+import FoodCard from "../../Components/FoodCard";
+import Paginations from "../../Components/Paginations";
 
 function AllFood() {
     const [foods, setFoods] = useState([]);
+    const [totalPage, setTotalPage] = useState(null);
+    const [activePage, setActivePage] = useState(1);
+    // Search Function
     const handleSearch = (e) => {
         e.preventDefault();
         const search = e.target.search.value;
@@ -14,15 +18,26 @@ function AllFood() {
         axios.get(`/foods?search=${search}`).then((res) => setFoods(res.data));
         e.target.reset();
     };
-
+    // get total page number
     useEffect(() => {
-        axios.get("/foods").then((res) => setFoods(res.data.reverse()));
+        axios
+            .get(`foods?page=${true}`)
+            .then((res) => setTotalPage(res.data.pages));
     }, []);
+    // get limited data according page number
+    useEffect(() => {
+        axios
+            .get(`/foods?activePage=${activePage}&pageNo=${activePage}`)
+            .then((res) => {
+                setFoods(res.data);
+            });
+    }, [activePage]);
 
     return (
         <div>
             <Title title={"All Foods"} />
 
+            {/* Search functionality */}
             <form
                 onSubmit={handleSearch}
                 className="flex items-center justify-center"
@@ -41,10 +56,20 @@ function AllFood() {
                 </div>
             </form>
 
+            {/* All Food */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-5 container mx-auto mt-10">
                 {foods.map((food) => (
                     <FoodCard key={food._id} food={food} />
                 ))}
+            </div>
+
+            {/* pagination */}
+            <div className="flex items-center justify-center mt-5 md:mt-10">
+                <Paginations
+                    total={totalPage}
+                    setActivePage={setActivePage}
+                    activePage={activePage}
+                />
             </div>
         </div>
     );
